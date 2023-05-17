b6852bb6db48ad83893d8914046fdfad0637f30e
imzhoujun <965105284@qq.com>
2023-05-18 00:32:28 +0800

    更新1

--- src/hooks/useMoment.ts
+++ src/hooks/useMoment.ts
@@ -2,9 +2,15 @@ import moment from 'moment'
 
 export const useMoment = () => {
   // timestamp时间戳 format时间格式如"YYYY年MM月DD日dddd"  "2023.01.17"  "2023-01-17"
-  //将时间戳转换成日期
-  const date = (timestamp: number, format: st) =>
-    moment.unix(timestamp).format(format) //年月日
+  //将时间戳 单位毫秒转换成日期
+  const dateMsec = (timestamp: number, format: st) => {
+    return moment(timestamp).format(format) //年月日
+  }
 
-  return { date }
+  //将时间戳 单位秒转换成日期
+  const dateSec = (timestamp: number, format: st) => {
+    return moment.unix(timestamp).format(format) //年月日
+  }
+
+  return { dateMsec, dateSec }
 }
